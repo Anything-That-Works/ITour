6af4abe04bba933acdaf4f2ebea912c5e786c9c3
Promal <promal.barua.cse@gmail.com>
2023-10-16 06:44:53 +0600

    Initial Commit

---
+++ ITour/ITourApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITourApp.swift
+//  ITour
+//
+//  Created by Promal on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ITourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
